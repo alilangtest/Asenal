21838192d246e5d1f314adbb1ba52f03da1e2991
baotiao <baotiao@gmail.com>
2015-01-04 14:45:52 +0800

    push

---
+++ tick/src/tick_conf.cc
@@ -0,0 +1,15 @@
+#include "sys/stat.h"
+#include "base_conf.h"
+#include "tick_conf.h"
+
+
+TickConf::TickConf(const char* path) :
+    BaseConf(path)
+{
+    getConfInt("port", &port_);
+    getConfInt("thread_num", &thread_num_);
+    getConfStr("log_path", log_path_);
+    getConfStr("qbus_cluster", qbus_cluster_);
+    getConfStr("qbus_topic", qbus_topic_);
+    getConfStr("qbus_conf_path", qbus_conf_path_);
+}
